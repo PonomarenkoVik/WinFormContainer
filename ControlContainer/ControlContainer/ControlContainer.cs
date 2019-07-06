bf006a5bfb94aa49ac1552924206bafb42a112c3
v.ponomarenko <Ponomarenkovic1979@gmail.com>
2019-07-06 17:01:29 +0300

    Add

---
+++ ControlContainer/ControlContainer/ControlContainer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace ControlContainer
+{
+    public class ControlContainer : Control
+    {
+        private int? _column;
+       
+
+        public ControlContainer()
+        {
+      
+        }
+
+
+        private int _columnSpan;
+        public virtual int ColumnSpan
+        {
+            get
+            {                
+                return _columnSpan;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                _columnSpan = value;
+            }
+        }
+
+
+        private int _rowSpan;
+        public virtual int RowSpan
+        {
+            get
+            {
+                return _rowSpan;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                _rowSpan = value;
+            }
+        }
+
+
+        public int? ColumnCount
+        {
+                get => _column;
+                set
+                {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                _column = value;
+                }
+        }
+
+        public int? RowCount
+        {
+            get => _column;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                _column = value;
+            }
+        }
+    }
+}
---
+++ ControlContainer/ControlContainer/ControlContainerCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.Control;
+
+namespace ControlContainer
+{
+    public class ControlContainerCollection : ControlCollection
+    {
+        public ControlContainerCollection(ControlContainer ctrl) : base(ctrl)
+        {
+
+        }
+
+        
+        public virtual ControlContainer this[int index] { get; }
+        
+        public virtual ControlContainer this[string key] { get; }
+
+
+    }
+}
