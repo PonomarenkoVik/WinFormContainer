92ae3d2446e2fb0ebd8fb35504f89c9e2cf45a93
v.ponomarenko <Ponomarenkovic1979@gmail.com>
2019-07-06 20:45:33 +0300

    2

--- ControlContainer/ControlContainer/ControlContainer.cs
+++ ControlContainer/ControlContainer/ControlContainer.cs
@@ -16,6 +16,7 @@ namespace ControlContainer
         public ControlContainer()
         {
             _controlGUID = Guid.NewGuid().ToString();
+            _columns = new List<Column>();
         }
 
 
@@ -47,6 +48,63 @@ namespace ControlContainer
         }
         #endregion
 
+        private void UpdateControls()
+        {
+            int notSettWidth = GetNotSettableColumnWidth();
+            int notSettHeight = GetNotSettableRowHeight();
+
+
+
+
+            foreach (var control in Controls)
+            {
+
+            }
+        }
+
+        private int GetNotSettableColumnWidth()
+        {
+            int totalWidth = 0;
+            int number = 0;
+            int widthWithoutPadding = this.Width - (Padding.Left + Padding.Right);
+            foreach (var column in _columns)
+            {
+                if (column.Width == 0)
+                    continue;
+                totalWidth += column.Width;
+                number++;
+            }
+
+            if (totalWidth > widthWithoutPadding)
+            {
+                throw new IndexOutOfRangeException("Total column width more then control size");
+            }
+
+            int notSetWidth = _columns.Count > 0 ? (widthWithoutPadding - totalWidth) / (_columns.Count - number) : widthWithoutPadding;
+            return notSetWidth;
+        }
+
+        private int GetNotSettableRowHeight()
+        {
+            int totalHeight = 0;
+            int number = 0;
+            int heightWithoutPadding = this.Height - (Padding.Top + Padding.Bottom);
+            foreach (var row in _rows)
+            {
+                if (row.Height == 0)
+                    continue;
+                totalHeight += row.Height;
+                number++;
+            }
+
+            if (totalHeight > heightWithoutPadding)
+            {
+                throw new IndexOutOfRangeException("Total row height more then control size");
+            }
+
+            int notSetHeigh = _rows.Count > 0 ? (heightWithoutPadding - totalHeight) / (_rows.Count - number) : heightWithoutPadding;
+            return notSetHeigh;
+        }
 
         private int _columnSpan;
         public int ColumnSpan
@@ -64,7 +122,6 @@ namespace ControlContainer
                 _columnSpan = value;
             }
         }
-
 
         private int _rowSpan;
         public int RowSpan
@@ -114,6 +171,19 @@ namespace ControlContainer
         public string ControlGUID
         {
             get => _controlGUID;
+        }
+
+
+        private readonly List<Column> _columns;
+        public List<Column> Columns
+        {
+            get => _columns;
+        }
+
+        private readonly List<Row> _rows;
+        public List<Row> Rows
+        {
+            get => _rows;
         }
     }
 }
